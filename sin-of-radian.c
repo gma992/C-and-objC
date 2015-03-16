3ca6d146f8caac46d135a29dcb08474c66d2e38b
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-16 22:56:43 +0100

    Create sin-of-radian.c

---
+++ sin-of-radian.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+// 
+//
+//  Created by Gabriel on 08/03/15.
+//  
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+
+    // display the sin of 1 radian and show the number rounded to three decimal points
+    // result = 0.841
+    
+     /*
+     // sine function: sin(x) = opposite / hypotenuse
+     // triangle with an angle x
+     */
+    
+    double  result = sin(1);
+    printf("the sin of 1 radian is %.3f \n", result);
+}
