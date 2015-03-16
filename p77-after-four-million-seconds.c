d86904f81966d45421e8cf2715024686bac5e9d0
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-16 23:00:46 +0100

    Create p77-after-four-million-seconds.c

---
+++ p77-after-four-million-seconds.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//
+//
+//  Created by Gabriel on 13/03/15.
+//
+//
+
+#include <stdio.h>
+//standart time library from C
+#include <time.h>
+
+int main(int argc, const char * argv[]) {
+    
+    //write a program that tells you the date after 4 million seconds
+    int fourMillionSeconds = now.tm_sec;
+    printf("At %d it has been 4 million of seconds since 1970", fourMillionSeconds);
+
+    return 0;
+}
