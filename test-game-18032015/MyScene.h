55b92b1a18efe27497c017931153d6524f247ffd
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-18 12:45:23 +0100

    Create MyScene.h

---
+++ test-game-18032015/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  test game 18032015
+//
+//  Created by Gabriel on 18/03/15.
+//  Copyright (c) 2015 gabrielmaldonado. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
