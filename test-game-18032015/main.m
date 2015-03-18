c53f48a0e4c2411929642ce5a012fe2508de985b
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-18 12:46:44 +0100

    Create main.m

---
+++ test-game-18032015/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test game 18032015
+//
+//  Created by Gabriel on 18/03/15.
+//  Copyright (c) 2015 gabrielmaldonado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
