40d2285b8a344efe7aa544e0dcc2efcbdefe3ea4
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-18 12:44:36 +0100

    Create TestGameViewController.h

---
+++ test-game-18032015/TestGameViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestGameViewController.h
+//  test game 18032015
+//
+//  Created by Gabriel on 18/03/15.
+//  Copyright (c) 2015 gabrielmaldonado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import "MyScene.h"
+
+@interface TestGameViewController : UIViewController
+
+@end
