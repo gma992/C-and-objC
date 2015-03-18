2e00902a1ed21711b1d73a406eecf9693b736809
gma992 <gabrielmaldonado.art@gmail.com>
2015-03-18 12:43:36 +0100

    Create AppDelegate.h

---
+++ test-game-18032015/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test game 18032015
+//
+//  Created by Gabriel on 18/03/15.
+//  Copyright (c) 2015 gabrielmaldonado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestGameViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
